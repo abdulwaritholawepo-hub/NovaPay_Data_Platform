f6b3c48f5255c0bd873a4f809de73fd3e515a442
Abdulwarith Olawepo Ayoola <abdulwaritholawepo@gmail.com>
2026-07-10 00:38:21 +0100

    Create initial SQL Server database schema

---
+++ database/create_tables.py
@@ -0,0 +1,86 @@
+from sqlalchemy import create_engine, text
+import pyodbc
+from urllib.parse import quote_plus
+
+connection_string = quote_plus(
+    r"DRIVER={ODBC Driver 18 for SQL Server};"
+    r"SERVER=DESKTOP-HIGKAHM\SQLEXPRESS;"
+    r"DATABASE=NovaPayDB;"
+    r"Trusted_Connection=yes;"
+    r"TrustServerCertificate=yes;"
+)
+
+engine = create_engine(f"mssql+pyodbc:///?odbc_connect={connection_string}")
+
+with engine.begin() as conn: 
+
+    conn.execute(
+        text("""
+             IF OBJECT_ID('dbo.customers', 'U') IS NULL
+             BEGIN
+             CREATE TABLE Customers(
+             customer_id INTEGER IDENTITY(1,1) PRIMARY KEY,
+             first_name  VARCHAR(50) NOT NULL,
+             last_name   VARCHAR(50) NOT NULL,
+             email       VARCHAR(100) UNIQUE NOT NULL,
+             phone_number VARCHAR(20) UNIQUE NOT NULL,
+             date_of_birth DATE NOT NULL,
+             gender      VARCHAR(10) NOT NULL,
+             account_number VARCHAR(20) UNIQUE NOT NULL,
+             wallet_balance DECIMAL(18,2) DEFAULT 0.00 NOT NULL,
+             account_status VARCHAR(20) NULL,
+             created_at     DATETIME2 NOT NULL
+             ) 
+             END
+             """)
+    
+    )
+
+    conn.execute(
+        text("""
+             IF OBJECT_ID('dbo.merchants', 'u') IS NULL
+             BEGIN
+             CREATE TABLE Merchants(
+             merchant_id INTEGER IDENTITY(1,1) PRIMARY KEY,
+             merchant_name VARCHAR(100) UNIQUE NOT NULL,
+             category VARCHAR(50) NULL,
+             email VARCHAR(100) UNIQUE NOT NULL,
+             phone_number VARCHAR(20) UNIQUE NOT NULL,
+             city VARCHAR(50) NULL,
+             state VARCHAR(50) NULL,
+             account_number VARCHAR(20) UNIQUE NOT NULL,
+             merchant_status VARCHAR(20) NULL,
+             created_at DATETIME2 NOT NULL
+             )
+             END
+             """)
+    )
+   
+
+    conn.execute(
+        text("""
+
+           
+
+             IF OBJECT_ID('dbo.transactions','U') IS NULL
+             BEGIN
+             CREATE TABLE Transactions(
+             transaction_id INTEGER IDENTITY(1,1) PRIMARY KEY,
+             sender_id INTEGER NOT NULL,
+             receiver_id INTEGER NOT NULL,
+             receiver_type VARCHAR(30) NULL,
+             amount DECIMAL(18,2) NOT NULL,
+             currency VARCHAR(10) NOT NULL,
+             payment_method VARCHAR(30) NULL,
+             transaction_category VARCHAR(30) NULL,
+             transaction_direction VARCHAR(10) NULL,
+             status VARCHAR(20) NULL,
+             reference_number VARCHAR(100) UNIQUE NOT NULL,
+             narration VARCHAR(255) NULL,
+             created_at DATETIME2 NOT NULL
+             )
+             END
+             """)
+    )
+
+print('customer, merchant and transactions table created successfullly')
