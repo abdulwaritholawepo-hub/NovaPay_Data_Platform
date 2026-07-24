af0a4e2a58ce869af39bd90d9acf89513dbcc693
Abdulwarith Olawepo Ayoola <abdulwaritholawepo@gmail.com>
2026-07-24 23:25:09 +0100

    Implement customer data transformation layer

---
+++ transform/customer_transformer.py
@@ -0,0 +1,53 @@
+import sys
+import os
+from datetime import datetime, date
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from extract.response_validator import validate_customer_data
+
+
+
+def transform_customers():
+    transform_customers_list  = []
+    validated_customers= validate_customer_data()
+    def clean_transform_value(key,val):
+        
+        if isinstance(val,str):
+            val = val.strip()
+
+            if key in{"first_name", "last_name"} and val:
+                return val.capitalize()
+            if key == "email" and val:
+                return val.lower()
+            if key == "gender" and val:
+                val = val.lower()
+                if val in {"male","m"}:
+                    return "Male"
+                elif val in {"female","f"}:
+                    return "Female"
+                else:
+                    return "Unknown"
+
+            try:
+                if key == "created_at" and val:
+                    val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S")
+                            
+                if key == "date_of_birth" and val:
+                    val = datetime.strptime(val, "%Y-%m-%d").date()
+                return val
+            except Exception:
+                raise
+        return val
+
+
+    for customer in validated_customers:
+        customer_transaform = {
+        key: clean_transform_value(key=key,val=value)
+        for key, value in customer.items()
+    
+        }
+        transform_customers_list.append(customer_transaform)
+    print(transform_customers_list)
+    return transform_customers_list
+        
+  
+transform_customers()
