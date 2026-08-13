2b2073e63e6f767852ba2c20c3fbd1a823b53ae0
Abdulwarith Olawepo Ayoola <abdulwaritholawepo@gmail.com>
2026-08-13 15:22:21 +0100

    Implement batch customer loading

--- processing/loader.py
+++ processing/loader.py
@@ -1,21 +1,32 @@
+from sqlalchemy import text
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from transform.customer_transformer import transform_customers
 from transform.transform_helpers import COLUMN_ORDER
-from sqlalchemy import text
 from sql_server_database.analytics_DB_connection import analytics_database_engine_connection
 
 engine = analytics_database_engine_connection()
-transformed_customer_data = transform_customers()
 
 columns = ", ".join(COLUMN_ORDER)
 parameters = ", ".join(f":{column}" for column in COLUMN_ORDER)
 insert_customers = text(f"""
-INSERT INTO customers({columns})
-VALUES({parameters})
-""")
+    INSERT INTO customers({columns})
+    VALUES({parameters})
+    """)
 
-with engine.begin() as conn:
-    conn.execute(insert_customers, transformed_customer_data)
-    print("cutomer data inserted succesfully")
+transformed_customer_data = transform_customers()
+customers = transformed_customer_data
+batch_size = 5
+start = 0
+batch_no = 0
+customer_length = len(customers)
+
+for customer in range(start, customer_length, batch_size):
+    customer_batch = customers[customer:customer + batch_size]
+    batch_no += 1
+    print(f"batch {batch_no}")
+
+    with engine.begin() as conn:
+        conn.execute(insert_customers, customer_batch)
+        print("cutomer data inserted succesfully")
