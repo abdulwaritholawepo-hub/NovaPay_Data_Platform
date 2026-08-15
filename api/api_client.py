dfc9e4256347a2f1a489a43f4f7902eddf948d6c
Abdulwarith Olawepo Ayoola <abdulwaritholawepo@gmail.com>
2026-08-15 12:47:07 +0100

    Implement API error handling and retry logic

--- api/api_client.py
+++ api/api_client.py
@@ -8,12 +8,11 @@ from config import api_config
 
 def get_customer_data(url=None, timeout=None, maximum_retries=None):
     if url is None:
-        url = api_config.customer_url
+        url = "https://httpbin.org/status/500"
     if timeout is None:
         timeout = api_config.timeout
     if maximum_retries is None:
         maximum_retries = api_config.maximum_retries
-    last_exception =None
     for attempts in range(1, maximum_retries + 1):
         try:
             response = requests.get(url, timeout=timeout )
@@ -22,20 +21,34 @@ def get_customer_data(url=None, timeout=None, maximum_retries=None):
             print("request succesful")
             data = response.json()
             return data
-        except (
-            requests.exceptions.HTTPError,
-            requests.exceptions.ConnectionError,
-            requests.exceptions.Timeout,
-            requests.exceptions.JSONDecodeError,
+        except requests.exceptions.HTTPError as http_error:
+            status_code = http_error.response.status_code
+            if 500 <= status_code <= 599:
+                if attempts == maximum_retries:
+                    print("maximum retries reached")
+                    raise http_error
+                else:
+                    wait_time = attempts**2
+                    print(f"retrying in {wait_time} seconds")
+                    time.sleep(wait_time)
+            else:
+                print(status_code)
+                raise http_error
+                
+            
+            
+        except  (
             requests.exceptions.RequestException,)as err: 
             last_exception = err
             error_type = type(last_exception).__name__
             print(f"{error_type} occurred: {last_exception}")
             if attempts == maximum_retries:
                 print("maximum retries reached")
+                raise last_exception
+                
             else:
                 wait_time = attempts**2
                 print(f"retrying in {wait_time} seconds")
                 time.sleep(wait_time)
-    if last_exception:
-        raise last_exception
+
+get_customer_data()
